865944d7df5785fd5f708fecace1728e54a5067d
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-16 15:30:15 +0100

    Task 5 print a string using printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+*    main - print usint printf
+*
+*    Description: This function prints a string using printf() function
+*    Return: 0 if the program succed
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
