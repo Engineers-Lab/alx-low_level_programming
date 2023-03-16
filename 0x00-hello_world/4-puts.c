506ed2865189100316aee312d48248c37f2db842
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-16 15:10:02 +0100

    task 4 v3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- *This function prints a string to console and and finishes with 0
- */
-
-int main(void){
+*    Description: This function prints a string to console
+*    Return: 0 if the program succed
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
