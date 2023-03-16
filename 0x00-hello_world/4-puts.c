59ad186e45b4cd4047605c73667404843c358d9c
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-16 15:03:32 +0100

    task 4 print using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+/**
+ *This function prints a string to console and and finishes with 0
+ */
+
 int main(void){
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
 }
