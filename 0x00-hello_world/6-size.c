beddeac2b4f9506e7c08253f7e43fd9a434a380f
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-16 16:04:20 +0100

    TASK 6..

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	print("Size of a char: %c byte(s)\n", sizeof(char));
 	print("Size of an int: %d byte(s)\n", sizeof(int));
 	print("Size of a long int: %ld byte(s)\n", sizeof(lintType));
-	print(("Size of long long int: %lld byte(s)\n", sizeof(llintType));
+	print("Size of long long int: %lld byte(s)\n", sizeof(llintType));
 	print("Size of float: %f byte(s)\n", sizeof(float));
 	return (0);
 }
