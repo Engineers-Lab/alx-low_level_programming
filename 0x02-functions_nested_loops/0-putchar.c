b4269c80a91564e9b0a42ce62da80e88972a0320
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-21 16:54:25 +0000

    Task 0 used only putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,14 @@
  */
 int main(void)
 {
-	char word[] = "_putchar";
-	int count = _count_word(word);
-	int i;
-	for (i = 0; i < count; i++)
-	{
-		_putchar("%s\n", word[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
        	return (0);
 }
