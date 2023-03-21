4b8bda5e6031b27ee06521bac7d5f0080f6de670
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-21 22:01:13 +0000

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isalpha - checks for numbers
+ *
+ *Return: 1 if true
+ */
+int _isalpha(int c)
+{
+	if (c > 47 && c < 59)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
