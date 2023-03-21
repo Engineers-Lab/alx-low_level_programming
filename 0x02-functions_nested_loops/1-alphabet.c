9bc0273952f67df26411c3b167759acbdde88bfa
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-21 21:33:23 +0000

    Task 1 fixed betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- *main - prints alphabet
+ *print_alphabet - prints alphabet
  *
+ *Description: Prints the alphabet in lowercase
  *Return: 0 if secceded
  */
 void print_alphabet(void)
 {
 	int i = 0;
+
 	for (i = 97; i < 123; i++)
 	{
 		_putchar(i);
