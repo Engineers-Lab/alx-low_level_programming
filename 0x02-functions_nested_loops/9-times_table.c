198eadabc3d3ed988ef8de6a0c6c52c3ea43dc26
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-21 23:29:59 +0000

    Task replaced i*j with there var

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,16 @@ void times_table(void)
 {
 	int i = 0;
 	int j = 0;
+	int res = 0;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar('0' + (j * i));
+			res = j * i;
+			_putchar('0' + res);
 			_putchar(44);
-			if (i * j > 9)
+			if (res > 9)
 			{
 				_putchar(32);
 				_putchar(32);
