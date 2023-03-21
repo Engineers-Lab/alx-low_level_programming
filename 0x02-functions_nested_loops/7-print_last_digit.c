04ecc937dc943b0e543dd697dcd5026849de3c30
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-21 22:48:09 +0000

    Task 7 added putchar to print the last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,12 @@ int print_last_digit(int n)
 	int last_digit = n % 10;
 	if (last_digit < 0)
 	{
+		_putchar('0' + (-1 * last_digit));
 		return (-1 * last_digit);
 	}
 	else
 	{
+		_putchar('0' + last_digit);
 		return (last_digit);
 	}
 }
