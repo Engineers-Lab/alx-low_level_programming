29b2e740967c9643f05ae603a1e70a21c7d69e7f
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-21 23:22:51 +0000

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *times_table - prints the multiplication table
+ *
+ *Return: void
+ */
+void times_table(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar('0' + j * i);
+			_putchar(',');
+			if (i * j > 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
