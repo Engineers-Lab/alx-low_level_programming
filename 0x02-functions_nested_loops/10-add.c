bf33815ea1bbb5a2451268246e0443d51711ec75
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-22 00:29:25 +0000

    Task 10 ix betty style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  *add - adds two integers
+ *@n: integer
+ *@x: integer
  *
  *Return: the result
  */
 int add(int n, int x)
 {
 	int result = n + x;
-	return result;
+	return (result);
 }
