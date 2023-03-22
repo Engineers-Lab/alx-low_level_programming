07c1ce117e98d8cb6f9fe82c659f1df1cdfcb900
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-22 00:04:35 +0000

    Task 9 refactor

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,22 +9,39 @@ void times_table(void)
 	int i = 0;
 	int j = 0;
 	int res = 0;
+	int first = 0;
+	int second = 0;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 5; j++)
+		for (j = 0; j < 10; j++)
 		{
 			res = j * i;
-			_putchar('0' +  res);
-			_putchar(44);
-			if (res > 9)
+			if (res < 10)
 			{
-				_putchar(32);
-				_putchar(32);
+				if (i > 0 && i < 9)
+				{
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar('0' +  res);
+				if (i < 9)
+					_putchar(44);
+			}
+			else if (res > 9)
+			{
+				if (i > 0 && i < 9)
+					_putchar(32);
+				first = res / 10;
+				second = res % 10;
+				_putchar(first + '0');
+				_putchar(second + '0');
+				if (i < 9)
+					_putchar(44);
 			}
 			else
 			{
-				_putchar(32);
+
 			}
 		}
 		_putchar('\n');
