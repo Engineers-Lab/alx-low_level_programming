b11ae47c6d94dc4aac78735d75b6b972add0dc2f
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-22 00:25:33 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *add - adds two integers
+ *
+ *Return: the result
+ */
+int add(int n, int x)
+{
+	int result = n + m;
+	int frst = 0;
+	int snd = 0;
+
+	if (resut < 10)
+	{
+		_putchar('0' + result);
+	}
+	else
+	{
+		frst = result / 10;
+		snd = result % 10;
+		_putchar('0' + frst);
+		_putchar('0' + snd);
+	}
+}
