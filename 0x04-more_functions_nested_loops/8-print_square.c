45e26a1887fefa5b4b661663e2ad6a3728e2f4c8
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-23 23:18:35 +0000

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_square - print a box of dashs
+ *@size: size of the box
+ *
+ *Return: nothing
+ */
+void print_square(int size)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		for (j = 0; j < n; j++)
+			_putchar(35);
+		i++;
+	}
+
+	_putchar('\n');
+}
