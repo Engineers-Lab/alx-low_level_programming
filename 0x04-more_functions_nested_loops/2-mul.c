162675ae759cc53a9f09cca4b79313b4ab309be2
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-23 22:00:22 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - factors tow nambers
+ *@a: first number
+ *@b: second number
+ *
+ *Return: the result
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
