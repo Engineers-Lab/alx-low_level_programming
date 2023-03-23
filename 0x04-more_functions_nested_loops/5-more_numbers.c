501b6ee8f402705c846feab7e0a892caf90f4e24
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-23 22:19:45 +0000

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *more_numbers - prints 10x base 10 numbers
+ *
+ *Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while(j < 10)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		j++;
+	}
+}
