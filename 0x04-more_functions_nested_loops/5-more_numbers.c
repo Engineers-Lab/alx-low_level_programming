bb10c543ad24668cf2b48e0264febe5e0c4a7152
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-23 22:25:55 +0000

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,14 @@ void more_numbers(void)
 	int i;
 	int j = 0;
 
-	while(j < 10)
+	while (j < 10)
 	{
 		for (i = 48; i < 58; i++)
+			_putchar(i);
+
+		for (i = 48; i < 53; i++)
 		{
+			_putchar(49);
 			_putchar(i);
 		}
 		_putchar('\n');
