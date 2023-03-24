4ca20270baf34f7a687593b5111ffb38aa8c8419
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-24 00:32:27 +0000

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		while (i < size)
 		{
-			for (j = size - 1; j > 1; j--)
+			for (j = size - i; j > 1; j--)
 				_putchar(32);
 			for (q = 0; q <= i; q++)
 				_putchar(35);
