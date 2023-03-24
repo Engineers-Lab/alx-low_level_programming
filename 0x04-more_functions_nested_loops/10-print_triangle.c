0a3109a99d22cb9b9bd241372f2887b8d33f4abb
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-24 00:01:50 +0000

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_diagonal - print given number to form a triangle
+ *@n: number of lines
+ *
+ *Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i = 0;
+	int j;
+
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			for (j = i; j >= 1; j--)
+				_putchar(32);
+			for (j = 0; j <= i; j++)
+				_putchar(35);
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
