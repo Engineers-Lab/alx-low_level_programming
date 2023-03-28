84611e9a6a41caebf998f132dfcbd9e189a8c2c0
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-28 00:13:03 +0000

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - print a nomber of elements from an array
+ *@a: array
+ *@n: nomber of elements
+ *
+ *Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int size;
+
+	size = (sizeof(a) / sizeof(a[0]));
+	for (i = n; i < size; i++)
+	{
+		if (i < size - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+}
