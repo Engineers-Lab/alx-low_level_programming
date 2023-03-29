e2a8a89e8d88a6922b7bb6d849b9b27989ab072d
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-29 17:22:47 +0000

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,12 +3,20 @@
  *_strncpy - copya given n char from a  string to another destination
  *@dest: destination
  *@src: source string
- *@n nomber of chars to be copied
+ *@n: nomber of chars to be copied
  *
+ *Return: pointer to copied string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+	int j = 0;
+
+	while (src[j] != '\0')
+		j++;
+	if (n > j)
+		n = j;
+
 
 	for (i = 0; i < n; i++)
 	{
