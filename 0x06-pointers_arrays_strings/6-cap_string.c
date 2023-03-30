ac671be77dcc36b2c570d052f7a97e4d61de1473
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-30 14:03:42 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *cap_string - capitalize each word of a given string
- *@c: the string
+ *@s: the string
  *
  *Return: pointer to the string
  */
@@ -10,15 +10,17 @@ char *cap_string(char *s)
 	int i = 0;
 	int j = 0;
 
-	while (s[j] !='\0')
+	while (s[j] != '\0')
 		j++;
 	for (i = 0; i < j; i++)
 	{
 		if (s[i] == ' ' || s[i] == '.' || s[i] == '\n' || s[i] == '!'
-		    || s[i] == '\t')
+		    || s[i] == '\t' || s[i] == ',' || s[i] == ';' || s[i] == '?'
+		    || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}'
+			|| s[i] == '"')
 		{
-			if (s[i+ 1] >= 'a' && s[i+1] <= 'z')
-				s[i+1] = s[i+1] - 32;
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] = s[i + 1] - 32;
 		}
 	}
 	return (s);
