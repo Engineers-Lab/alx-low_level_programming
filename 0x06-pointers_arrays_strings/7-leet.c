22cd889acebc2bf2f0e9269c49a6982ecf8a8caf
Engineers-Lab <ais.maazaz@gmail.com>
2023-03-30 20:31:02 +0000

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *leet - change some chars to numbers
+ *@s: the given string
+ *
+ *Return: pointer to the string
+ */
+char *leet(char *s)
+{
+	char lowAlpha[] = {'a', 'e', 'o', 't', 'l'};
+	char upperAlpha[] = {'A', 'E', 'O', 'T', 'L'};
+	int repNum[] = {4, 3, 0, 7, 1};
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == lowAlpha[j] || s[i] == upperAlpha[j])
+				s[i] = repNum[j];
+		}
+		i++;
+	}
+	return (s);
+}
