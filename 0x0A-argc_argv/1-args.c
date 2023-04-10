01b4bf99b97168638a923e6721e90b9d447cb239
Engineers-Lab <ais.maazaz@gmail.com>
2023-04-10 23:16:35 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *@argc: nb of arguments
+ *@argv: args vectors
+ *
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
