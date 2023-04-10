5f4409ae77ca8c67471a9f31d8bb5c7a4586a250
Engineers-Lab <ais.maazaz@gmail.com>
2023-04-10 23:44:11 +0000

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - prints the production of 2 nombers
+ *@argc: args nomber
+ *@argv: args vector
+ *
+ *Return: 0 if success othwise 1
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int result;
+
+	if (argc <= 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		result = argv[1] * argv[2];
+		printf("%d\n", result);
+	}
+}
