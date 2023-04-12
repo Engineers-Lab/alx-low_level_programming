7efebe928f4f4b2d5a5c17bc1b8df4d82426a07b
Engineers-Lab <ais.maazaz@gmail.com>
2023-04-12 20:50:54 +0000

    Task 2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	pint = malloc(sizeof(int *) * height);
-	for (w = 0; w < width; w++)
+	for (w = 0; w < height; w++)
 		pint[w] = malloc(sizeof(int) * width);
 	if (pint == NULL)
 	{
