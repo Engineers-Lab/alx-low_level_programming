9a37e8a278c3a836ef74b2d0221a8cd3003f1ec2
Engineers-Lab <ais.maazaz@gmail.com>
2023-04-12 00:07:35 +0000

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *alloc_grid - create a 2D array
+ *@width: rows
+ *@height: columns
+ *
+ *Return:NULL on failure or 0 or negative dimensions
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, w;
+	int **pint;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	pint = malloc(sizeof(int) * width);
+	for (w = 0; w < width; w++)
+		pint[w] = malloc(sizeof(int) * height);
+	if (pint == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			pint[i][j] = 0;
+		}
+	}
+	return (pint);
+}
