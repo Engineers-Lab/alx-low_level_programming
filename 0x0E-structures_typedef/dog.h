b767b6a20866b59a0487b2711b3f07fee326b0f5
Engineers-Lab <ais.maazaz@gmail.com>
2023-04-17 23:18:21 +0000

    Task 0x0E.0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - holds the general information obout a given dog
+ *@name: the dog's name
+ *@age: the dog's age
+ *@owner: the dog's owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
