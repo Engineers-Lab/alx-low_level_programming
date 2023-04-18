e84ab2a72b3ac33ee0824c638793d23f300a9546
Engineers-Lab <ais.maazaz@gmail.com>
2023-04-18 00:05:15 +0000

    Task 0x0E.5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *free_dog - free memory allocated for dog struct
+ *@d: pointer to struct to be freed
+ *
+ *Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
