aef66375e4fa0226c3d3d48d201f37d3591d7a50
Engineers-Lab <ais.maazaz@gmail.com>
2023-04-19 21:26:55 +0000

    Task 0x0F.1 betty fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
 /**
  *array_iterator - apply a given function on each element of the array
  *@array: pointer to a given array
- *@size_t: array size
+ *@size: array size
  *@action: the function to be applied
  *
  *Return: nothing
